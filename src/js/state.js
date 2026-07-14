365f7493be952debbd19276a871dc0c835f84d8e
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-14 03:00:00 +0200

    Apply linting

--- src/js/state.js
+++ src/js/state.js
@@ -1,38 +1,38 @@
 export default class State {
-    #location;
-    #unit;
-    #currentData
+	#location;
+	#unit;
+	#currentData;
 
-    constructor(location = null, unit = "C", currentData = {}) {
-        this.#location = location;
-        this.#unit = unit;
-        this.#currentData = currentData;
-    }
+	constructor(location = null, unit = "C", currentData = {}) {
+		this.#location = location;
+		this.#unit = unit;
+		this.#currentData = currentData;
+	}
 
-    get location() {
-        return this.#location;
-    }
+	get location() {
+		return this.#location;
+	}
 
-    set location(newLocation) {
-        this.#location = newLocation;
-    }
+	set location(newLocation) {
+		this.#location = newLocation;
+	}
 
-    get unit() {
-        return this.#unit;
-    }
+	get unit() {
+		return this.#unit;
+	}
 
-    set unit(newUnit) {
-        if (newUnit !== 'C' && newUnit !== 'F') {
-        throw new Error('Unit must be "C" or "F"');
-        }
-        this.#unit = newUnit;
-    }
+	set unit(newUnit) {
+		if (newUnit !== "C" && newUnit !== "F") {
+			throw new Error('Unit must be "C" or "F"');
+		}
+		this.#unit = newUnit;
+	}
 
-    get currentData() {
-        return this.#currentData;
-    }
+	get currentData() {
+		return this.#currentData;
+	}
 
-    set currentData(newData) {
-        this.#currentData = newData;
-    }
+	set currentData(newData) {
+		this.#currentData = newData;
+	}
 }
