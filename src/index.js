6b85698749cecd5d8100e98d125670967db3193e
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-16 04:39:26 +0200

    Add compressed video assets

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { createIcons, Search, MapPin, Droplets, Wind, Sun, Sunrise, Sunset, CloudRain } from 'lucide';
+
+createIcons({
+  icons: { Search, MapPin, Droplets, Wind, Sun, Sunrise, Sunset }
+});
